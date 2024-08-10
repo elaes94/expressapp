ac6f72224f85bf76f0997ff3121b219192f5ab33
LasHomePC <lassana.traore@akkodis.com>
2024-08-10 10:16:41 +0200

    add netlify cli

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+import handler from "./netlify/functions/api"
+
 var app = express();
 
 // view engine setup
@@ -20,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/', handler);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
